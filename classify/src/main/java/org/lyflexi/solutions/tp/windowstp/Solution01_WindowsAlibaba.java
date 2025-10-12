908b42ce75a232b728675614f7dd7a6e47da42ca
lltopk <liuyanoutsee@outlook.com>
2025-10-12 14:17:05 +0800

    update bis

---
+++ classify/src/main/java/org/lyflexi/solutions/tp/windowstp/Solution01_WindowsAlibaba.java
@@ -0,0 +1,65 @@
+package org.lyflexi.solutions.tp.windowstp;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*滑动窗口：阿里巴巴找黄金宝箱(V)：滑动窗口
+* 难度	易
+题目说明	一贫如洗的樵夫阿里巴巴在去砍柴的路上，无意中发现了强盗集团的藏宝地，藏宝地有编号从0-N的箱子，每个箱子上面贴有一个数字。
+阿里巴巴念出一个咒语数字 k ( k<N )，找出连续 k 个宝箱数字和的最大值，并输出该最大值。
+输入描述	第一行输入一个数字字串，数字之间使用逗号分隔，例如: 2,10,-3,-8,40,5。
+1 ≤ 字串中数字的个数 ≤ 100000。
+-10000 ≤ 每个数字 ≤10000。
+第二行输入咒语数字，例如: 4，咒语数字大小小于宝箱的个数。
+输出描述	连续 k 个宝箱数字和的最大值，例如: 39。
+补充说明	无
+------------------------------------------------------
+示例
+示例1
+输入	2,10,-3,-8,40,5
+* 4
+输出	39
+说明	无
+*
+示例2
+输入	8
+1
+输出	8
+说明	无
+
+*
+*
+*
+*
+* */
+
+public class Solution01_WindowsAlibaba {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] nums = Arrays.stream(in.nextLine().split(",")).mapToInt(Integer::parseInt).toArray();
+        int k = Integer.parseInt(in.nextLine());
+        System.out.println(maximumSubarraySum(nums, k));
+        return;
+    }
+
+    //滑动窗口，那就是数组+for循环
+    public static long maximumSubarraySum(int[] nums, int k) {
+        int n = nums.length;
+        long rst = 0, sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        //比较函数需要两个参数，rst暂存函数结果，sum暂存迭代窗口
+        rst = Math.max(rst, sum);
+        for (int i = k; i < n; i++) {
+            // 右窗口右移
+            sum += nums[i];
+            // 左窗口右移
+            sum -= nums[i - k];
+            rst = Math.max(rst, sum);
+        }
+        return rst;
+    }
+
+}
---
+++ classify/src/main/java/org/lyflexi/solutions/tp/windowstp/Solution03_UnfixedWindowUniqueStrContinuous.java
@@ -0,0 +1,67 @@
+package org.lyflexi.solutions.tp.windowstp;
+
+import java.util.*;
+
+/**
+ * @Author: ly
+ * @Date: 2024/2/4 10:19
+ */
+
+/*
+3. 无重复字符的最长子串
+
+
+给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+
+示例 1:
+输入: s = "abcabcbb"
+输出: 3
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+
+示例 2:
+输入: s = "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+
+示例 3:
+输入: s = "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。*/
+public class Solution03_UnfixedWindowUniqueStrContinuous {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+    public static int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        // 哈希集合，记录每个字符是否出现过
+        Set<Character> occupy = new HashSet<>();
+
+        int left = 0, right = 1, ans = 0;
+        occupy.add(s.charAt(0));
+        while(right<n){
+            if (occupy.contains(s.charAt(right))) {
+                //occupy清空为下一轮比较做准备
+                occupy.clear();
+                //左指针移动1位
+                left ++;
+                //右指针回来
+                right = left;
+            }
+
+            // 正常情况下，不断地移动右指针
+            occupy.add(s.charAt(right));
+            ans = Math.max(ans, right - left + 1);
+            right++;
+        }
+        return ans;
+    }
+}
