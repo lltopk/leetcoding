371e55d6334c803e1d1242ccaebb73053dc4a471
lltopk <liuyanoutsee@outlook.com>
2025-10-12 16:44:01 +0800

    update bis

---
+++ c.java
@@ -0,0 +1,8 @@
+/**
+ * @author hasee
+ * @version V1.00
+ * @time 2025/10/12 16:42
+ * @description
+ */
+public class c {
+}
---
+++ classify/src/main/java/org/lyflexi/test/Test06.java
@@ -0,0 +1,26 @@
+package org.lyflexi.test;
+
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Test06 {
+    private Entry[] ets;
+    public Test06() {
+        ets = new Entry[]{};
+    }
+
+    public void set(String key, String value, int timestamp) {
+
+    }
+
+    public String get(String key, int timestamp) {
+
+    }
+    class Entry{
+        public String k;
+        public String v;
+        public Integer timestamp;
+    }
+
+}
