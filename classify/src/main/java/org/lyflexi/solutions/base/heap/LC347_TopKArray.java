1c3bb4625b7991abc9d2baa73f9607060736c963
lltopk <liuyanoutsee@outlook.com>
2025-12-21 12:17:51 +0800

    update struct

---
+++ classify/src/main/java/org/lyflexi/solutions/base/heap/LC347_TopKArray.java
@@ -0,0 +1,102 @@
+package org.lyflexi.solutions.base.heap;
+
+import java.util.*;
+
+/**
+ * @Author: ly
+ * @Date: 2024/2/10 17:17
+ */
+
+/*347. 前 K 个高频元素
+给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
+
+示例 1:
+输入: nums = [1,1,1,2,2,3], k = 2
+输出: [1,2]
+
+示例 2:
+输入: nums = [1], k = 1
+输出: [1]*/
+public class LC347_TopKArray {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int[] nums = Arrays.stream(scanner.nextLine().split(",")).mapToInt(Integer::parseInt).toArray();
+        int k = Integer.parseInt(scanner.nextLine());
+
+        for (int i : topKByPriotity(nums, k)) {
+            System.out.println(i);
+        }
+
+
+    }
+
+    /*优化，不全排序，利用Java的优先队列PriorityQueue-堆结构默认只排序堆中已有的元素个数
+    *
+    * 条件控制priorityQueue的容量为k即可满足，nlogk的复杂度
+    * */
+    public static int[] topKByPriotity(int[] nums, int k) {
+
+        int n = nums.length;
+
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int key = nums[i];
+            map.put(key,map.getOrDefault(key,0)+1);
+        }
+
+        PriorityQueue<Map.Entry<Integer,Integer>> priorityQueue = new PriorityQueue<>(
+                (entry1,entry2)->entry1.getValue()-entry2.getValue()//小顶堆，方便取堆中最小元素进行淘汰
+        );
+
+        for (Map.Entry<Integer,Integer> entry:map.entrySet()){
+            Integer key = entry.getKey();
+            Integer value = entry.getValue();
+
+            if (priorityQueue.size()==k){
+                //淘汰策略，当堆中最小元素频次小于当前entry的频次
+                Map.Entry<Integer, Integer> peek = priorityQueue.peek();
+                if (peek.getValue()<value){
+                    priorityQueue.poll();
+                    priorityQueue.add(entry);
+                }
+
+            }else {
+                priorityQueue.add(entry);
+            }
+
+        }
+
+        int[] topK = new int[k];
+
+        for (int i = k-1; i >=0; i--) {//堆中确实是前k大的元素集合，但由于是小顶堆，出堆顺序是前key元素的逆序
+            topK[i] = priorityQueue.poll().getKey();
+        }
+        return topK;
+
+    }
+
+    /*使用了排序API，nlogn的复杂度*/
+/*    public static int[] topKFrequent(int[] nums, int k) {
+
+        int n = nums.length;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int key = nums[i];
+            map.put(key,map.getOrDefault(key,0)+1);
+        }
+
+        ArrayList<Map.Entry<Integer,Integer>> list = new ArrayList<>(map.entrySet());
+
+        List<Map.Entry<Integer, Integer>> sortedList = list.stream().sorted((item1, item2) -> item2.getValue() - item1.getValue()).collect(Collectors.toList());
+
+        int[] topK = new int[k];
+        for (int i = 0; i < k; i++) {
+            topK[i] = sortedList.get(i).getKey();
+        }
+        return topK;
+    }*/
+}
---
+++ classify/src/main/java/org/lyflexi/solutions/base/slipwindows/LC647_countsPalindromeSubStr.java
@@ -0,0 +1,44 @@
+package org.lyflexi.solutions.base.slipwindows;
+
+/**
+ * @Author: ly
+ * @Date: 2024/3/23 22:18
+ */
+
+/*
+* 647. 回文子串 双指针（中心扩散法）
+给你一个字符串 s ，请你统计并返回这个字符串中 回文子串 的数目。
+回文字符串 是正着读和倒过来读一样的字符串。
+子字符串 是字符串中的由连续字符组成的一个序列。
+具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被视作不同的子串。
+
+示例 1：
+输入：s = "abc"
+输出：3
+解释：三个回文子串: "a", "b", "c"
+*
+示例 2：
+输入：s = "aaa"
+输出：6
+解释：6个回文子串: "a", "a", "a", "aa", "aa", "aaa"
+* */
+public class LC647_countsPalindromeSubStr {
+    public int countSubstrings(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            count += countPalindromes(s, i, i); // 以当前字符为中心的回文串（奇数长度）
+            count += countPalindromes(s, i, i + 1); // 以当前字符和下一个字符之间的空隙为中心的回文串（偶数长度）
+        }
+        return count;
+    }
+    // 计算以指定位置为中心的回文串数量
+    private int countPalindromes(String s, int left, int right) {
+        int count = 0;
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            count++;
+            left--;
+            right++;
+        }
+        return count;
+    }
+}
