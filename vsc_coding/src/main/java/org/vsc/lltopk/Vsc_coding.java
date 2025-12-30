b0b5e16fb1c781db236bef97ffe8a3151b1bcff8
lltopk <liuyanoutsee@outlook.com>
2025-12-30 21:26:26 +0800

    add module with vsc

---
+++ vsc_coding/src/main/java/org/vsc/lltopk/Vsc_coding.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.vsc.lltopk;
+
+/**
+ *
+ * @author hasee
+ */
+public class Vsc_coding {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
