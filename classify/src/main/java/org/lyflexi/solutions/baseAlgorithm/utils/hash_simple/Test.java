f5e82688b8aa613e7f89fdd7b0ab3132c75c54ad
lltopk <liuyanoutsee@outlook.com>
2026-05-10 15:14:02 +0800

    struct

---
+++ classify/src/main/java/org/lyflexi/solutions/baseAlgorithm/utils/hash_simple/Test.java
@@ -0,0 +1,12 @@
+package org.lyflexi.solutions.baseAlgorithm.utils.hash_simple;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+    public static void main(String[] args) {
+        Map<Integer, Integer> helper = new HashMap<>();
+        helper.merge(1, 1, (a,b)-> {return a+b;});
+        helper.merge(1, 1, Integer::sum);
+    }
+}
