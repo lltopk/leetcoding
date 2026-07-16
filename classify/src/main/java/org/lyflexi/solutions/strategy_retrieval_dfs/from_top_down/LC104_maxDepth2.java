6aa0d44cbf8e82627209b5926d236b55bd355ba1
lltopk <liuyanoutsee@outlook.com>
2026-07-16 22:35:47 +0800

    feat dfs

--- classify/src/main/java/org/lyflexi/solutions/strategy_retrieval_dfs/from_top_down/LC104_maxDepth2.java
+++ classify/src/main/java/org/lyflexi/solutions/strategy_retrieval_dfs/from_top_down/LC104_maxDepth2.java
@@ -47,18 +47,26 @@ import org.lyflexi.common.TreeNode;
 /**
  * 自顶向下DFS
  */
-public class LC104_maxDepth {
-    int ret = 0;
+public class LC104_maxDepth2 {
     public int maxDepth(TreeNode root) {
-        dfs(root, 0);
-        return ret;
+        //创建一个引用数组
+        int[] ret = new int[1];
+        dfs(root, ret, 0);
+        return ret[0];
     }
-    private void dfs(TreeNode root, int depth){
+
+    /**
+     * 如果要传结果进去, 不能是普通变量int, 必须是引用类型int[]
+     * @param root
+     * @param ret
+     * @param depth
+     */
+    private void dfs(TreeNode root, int[] ret, int depth){
         if(root == null){
             return;
         }
-        ret = Math.max(++depth, ret);
-        dfs(root.left, depth);
-        dfs(root.right, depth);
+        ret[0] = Math.max(++depth, ret[0]);
+        dfs(root.left, ret, depth);
+        dfs(root.right, ret, depth);
     }
 }
