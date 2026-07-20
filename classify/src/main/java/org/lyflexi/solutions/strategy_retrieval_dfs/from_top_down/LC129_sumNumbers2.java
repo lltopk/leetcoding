443c935f4f0aecefdc1147c6e03a9bc16eabbb14
lltopk <liuyanoutsee@outlook.com>
2026-07-20 12:14:02 +0800

    feat dfs

---
+++ classify/src/main/java/org/lyflexi/solutions/strategy_retrieval_dfs/from_top_down/LC129_sumNumbers2.java
@@ -0,0 +1,79 @@
+package org.lyflexi.solutions.strategy_retrieval_dfs.from_top_down;
+
+/**
+ * 129. 求根节点到叶节点数字之和
+ * 已解答
+ * 中等
+ * 相关标签
+ * premium lock icon
+ * 相关企业
+ * 给你一个二叉树的根节点 root ，树中每个节点都存放有一个 0 到 9 之间的数字。
+ * 每条从根节点到叶节点的路径都代表一个数字：
+ *
+ * 例如，从根节点到叶节点的路径 1 -> 2 -> 3 表示数字 123 。
+ * 计算从根节点到叶节点生成的 所有数字之和 。
+ *
+ * 叶节点 是指没有子节点的节点。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,2,3]
+ * 输出：25
+ * 解释：
+ * 从根到叶子节点路径 1->2 代表数字 12
+ * 从根到叶子节点路径 1->3 代表数字 13
+ * 因此，数字总和 = 12 + 13 = 25
+ * 示例 2：
+ *
+ *
+ * 输入：root = [4,9,0,5,1]
+ * 输出：1026
+ * 解释：
+ * 从根到叶子节点路径 4->9->5 代表数字 495
+ * 从根到叶子节点路径 4->9->1 代表数字 491
+ * 从根到叶子节点路径 4->0 代表数字 40
+ * 因此，数字总和 = 495 + 491 + 40 = 1026
+ *
+ *
+ * 提示：
+ *
+ * 树中节点的数目在范围 [1, 1000] 内
+ * 0 <= Node.val <= 9
+ * 树的深度不超过 10
+ *
+ * 面试中遇到过这道题?
+ * 1/5
+ * 是
+ * 否
+ * 通过次数
+ * 392,571/545.6K
+ * 通过率
+ * 72.0%
+ */
+
+import org.lyflexi.common.TreeNode;
+
+/**
+ * 自顶向下 非回溯
+ */
+public class LC129_sumNumbers2 {
+    int sum = 0;
+    public int sumNumbers(TreeNode root) {
+        dfs(root, 0);
+        return sum;
+    }
+    private void dfs(TreeNode root, int x){
+        if(root == null){
+            return;
+        }
+        x = x* 10 + root.val;
+        if(root.left == null && root.right == null){
+            sum += x;
+        }
+        dfs(root.left, x);
+        dfs(root.right, x);
+    }
+}
